369464a34630061416d52e021ac0d8925c711879
Jina Zhu <jinazhu87@gmail.com>
2021-01-04 13:44:21 -0800

    design homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 
+import clappingBoard from "../../images/clapping-board.svg";
+import search from "../../images/search.svg";
+import clearIcon from "../../images/clear-icon.svg";
+import {
+  HomeContainer,
+  ClapperText,
+  ClapperInfo,
+  SearchContainer,
+  SearchBar,
+  ClearButton,
+} from "./styled";
+
 const Home = () => {
   return (
-    <div>
-      <p>I am home</p>
-    </div>
+    <HomeContainer>
+      <img src={clappingBoard} alt="clapping board" width="100%" />
+      <ClapperInfo>
+        <ClapperText margin={"20px 0 5px 0"}>THE SHOPPIES</ClapperText>
+        <SearchContainer>
+          <img src={search} alt="search icon" width="80" />
+          <SearchBar type="text" placeholder="Search Movie" />
+          <ClearButton>
+            <img src={clearIcon} alt="clear icon" width="80" />
+          </ClearButton>
+        </SearchContainer>
+      </ClapperInfo>
+    </HomeContainer>
   );
 };
 
